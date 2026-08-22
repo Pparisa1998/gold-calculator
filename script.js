79d99a3513a715c141517a9836fe0a6bdbb92b9a
Pparisa1998 <113744155+Pparisa1998@users.noreply.github.com>
2026-08-22 15:38:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,29 +13,44 @@ function calculate() {
 
     const result = document.getElementById("result");
 
+    /* ถ้ากรอกข้อมูลไม่ครบ ให้ซ่อนผลลัพธ์ */
     if (!goldPrice || !goldPercent || !goldWeight) {
-        result.innerHTML = "กรอกข้อมูลไม่ครบ";
+        result.classList.remove("show");
+        result.innerHTML = "";
         return;
     }
 
-    // ขั้นที่ 1: ราคาทอง × 0.656
+    /* ขั้นที่ 1: ราคาทอง × 0.656 */
     const pricePerGram = goldPrice * 0.656;
 
-    // ขั้นที่ 2: คำนวณตามเปอร์เซ็นต์
+    /* ขั้นที่ 2: คำนวณตามเปอร์เซ็นต์ทอง */
     const priceByPercent =
         pricePerGram * (goldPercent / 100);
 
-    // ขั้นที่ 3: คูณน้ำหนัก
+    /* ขั้นที่ 3: คูณน้ำหนักทอง */
     const priceByWeight =
         priceByPercent * goldWeight;
 
-    // ขั้นที่ 4: เลื่อนจุดทศนิยม 1 ตำแหน่ง
+    /* ขั้นที่ 4: หาร 10 */
     const total = priceByWeight / 10;
 
+    /* แสดงกล่องผลลัพธ์ */
+    result.classList.add("show");
+
+    /* แสดงผลลัพธ์ */
     result.innerHTML = `
+        <div>
+            ราคาต่อกรัม:
+            ${pricePerGram.toLocaleString("th-TH", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })} บาท
+        </div>
+
+        <br>
 
         <strong>
-            ราคา: 
+            ราคาสุดท้าย:
             ${total.toLocaleString("th-TH", {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2
