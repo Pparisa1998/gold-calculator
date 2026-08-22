e698f096945ca90cc95e6a21e7c454198f8748e5
Pparisa1998 <113744155+Pparisa1998@users.noreply.github.com>
2026-08-22 19:23:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,50 @@ async function updateGoldPrice() {
 
 // โหลดราคาทองเมื่อเปิดเว็บ
 updateGoldPrice();
+function calculate() {
+    const goldPrice = Number(
+        document.getElementById("goldPrice").value
+    );
+
+    const goldPercent = Number(
+        document.getElementById("goldPercent").value
+    );
+
+    const goldWeight = Number(
+        document.getElementById("goldWeight").value
+    );
+
+    const result = document.getElementById("result");
+
+    /* ถ้ากรอกข้อมูลไม่ครบ ให้ซ่อนผลลัพธ์ */
+    if (!goldPrice || !goldPercent || !goldWeight) {
+        result.classList.remove("show");
+        result.innerHTML = "";
+        return;
+    }
+
+    /* คำนวณราคาทอง */
+    const total =
+        goldPrice *
+        0.656 *
+        (goldPercent / 100) *
+        goldWeight /
+        10;
+
+    /* แสดงกล่องผลลัพธ์ */
+    result.classList.add("show");
+
+    /* แสดงเฉพาะราคาสุดท้าย */
+    result.innerHTML = `
+        <strong>
+            ราคาสุดท้าย:
+            ${total.toLocaleString("th-TH", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })} บาท
+        </strong>
+    `;
+}
 
 
 // อัปเดตราคาทุก 1 นาที
